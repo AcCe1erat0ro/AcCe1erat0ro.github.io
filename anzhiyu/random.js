11ebe5bae28a84100d5080bc3bf3f92a8bb6db14
Accelerat0r <acceleratorclb@sina.com>
2024-08-19 20:43:39 +0800

    Site updated: 2024-08-19 20:43:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/19/Linux系统编程入门/","2024/08/19/01Linux系统编程/","2024/08/19/hello-world/"];function toRandomPost(){
+var posts=["2024/08/19/Linux系统编程入门/","2024/08/19/01Linux系统编程/","2024/08/19/hello-world/","2024/08/19/test01/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
